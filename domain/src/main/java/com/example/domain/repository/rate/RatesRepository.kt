5038d0b9fa109eca6f8e8fd1c112b53cb18fb6d6
Marcos Fuentes Aguilera <marcos.fuentes.aguilera@everis.com>
2020-01-19 17:39:41 +0100

    Optimized imports

--- domain/src/main/java/com/example/domain/repository/rate/RatesRepository.kt
+++ domain/src/main/java/com/example/domain/repository/rate/RatesRepository.kt
@@ -2,8 +2,7 @@ package com.example.domain.repository.rate
 
 import androidx.lifecycle.LiveData
 import com.example.domain.models.RateEntity
-import com.example.domain.network.ResultState
 
 interface RatesRepository {
-    fun getRates(): LiveData<ResultState<List<RateEntity>>>
+    fun getRates(): LiveData<Result<List<RateEntity>>>
 }
--- domain/src/main/java/com/example/domain/repository/transaction/TransactionsRepository.kt
+++ domain/src/main/java/com/example/domain/repository/transaction/TransactionsRepository.kt
@@ -1,10 +1,9 @@
 package com.example.domain.repository.transaction
 
 import androidx.lifecycle.LiveData
-import com.example.domain.models.RateEntity
 import com.example.domain.models.TransactionEntity
-import com.example.domain.network.ResultState
+
 
 interface TransactionsRepository {
-    fun getTransactions(): LiveData<ResultState<List<TransactionEntity>>>
+    fun getTransactions(): LiveData<Result<List<TransactionEntity>>>
 }
