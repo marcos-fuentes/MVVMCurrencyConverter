b333b33475bb2887320681cfbb697ea9347363f6
Marcos Fuentes Aguilera <marcos.fuentes.aguilera@everis.com>
2020-01-21 15:40:19 +0100

    Created fragmentargs for detail product fragment

--- presentation/src/main/java/com/example/presentation/products/fragment/ProductDetailFragment.kt
+++ presentation/src/main/java/com/example/presentation/products/fragment/ProductDetailFragment.kt
@@ -2,29 +2,18 @@ package com.example.presentation.products.fragment
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.domain.models.entity.RateEntity
 import com.example.domain.models.entity.TransactionEntity
 import com.example.presentation.R
 import com.example.presentation.base.BaseFragment
-import com.example.presentation.mapper.ProductViewMapper
 import com.example.presentation.model.ProductUIModel
-import com.example.presentation.products.adapter.ProductsAdapter
-import com.example.presentation.products.viewmodel.ProductViewModel
+import com.example.presentation.model.TransactionUIModel
 import kotlinx.android.synthetic.main.product_fragment.*
-import org.koin.android.ext.android.inject
-import org.koin.android.viewmodel.ext.android.viewModel
-import java.io.Serializable
 
 class ProductDetailFragment : BaseFragment() {
 
-    //val args:ProductFragmentArgs by navArgs()
-
-    private var transactionList: List<TransactionEntity?> = ArrayList()
-    private var rateList: List<RateEntity?> = ArrayList()
-    private var productList: ArrayList<ProductUIModel> = ArrayList()
+    private var transactionList: ArrayList<TransactionUIModel> = ArrayList()
     //private var productAdapter: ProductsAdapter = ProductsAdapter(context, productList)
 
     override fun observeViewModel() {
@@ -33,26 +22,28 @@ class ProductDetailFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        //args.product
+        if (arguments != null) {
+            val productUIModel: ProductUIModel =
+                ProductDetailFragmentArgs.fromBundle(arguments!!).productDetail
+        }
     }
 
 
     private fun setProductList() {
         if (transactionList.isNotEmpty() && rateList.isNotEmpty()) {
-           // productList = productViewMapper.getProductsFromTransaction(transactionList, rateList)
+            // productList = productViewMapper.getProductsFromTransaction(transactionList, rateList)
             updateRecyclerViewList()
         }
     }
 
     override fun getLayout(): Int {
-        return R.layout.product_fragment
+        return R.layout.product_detail_fragment
     }
 
     private fun updateRecyclerViewList() {
         //productAdapter = ProductsAdapter(context, productList)
         rvProducts.layoutManager = LinearLayoutManager(context)
-     //   rvProducts.adapter = productAdapter
-     //   productAdapter.notifyDataSetChanged()
+        //   rvProducts.adapter = productAdapter
+        //   productAdapter.notifyDataSetChanged()
     }
 }
